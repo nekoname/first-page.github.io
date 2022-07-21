77c70d864150c8f1c5f60354f5fca56f333f5019
neko-extasis <93204226+nekoname@users.noreply.github.com>
2022-07-21 10:56:26 +0200

    f

---
+++ includes/footer.php
@@ -0,0 +1,88 @@
+<div class="container">
+      <div class="row">
+      <div class="col">
+        <a href="#" class="text-decoration-none text-dark">
+          <svg width="120px" height="180px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M560 448H512V113.5c0-27.25-21.5-49.5-48-49.5L352 64.01V128h96V512h112c8.875 0 16-7.125 16-15.1v-31.1C576 455.1 568.9 448 560 448zM280.3 1.007l-192 49.75C73.1 54.51 64 67.76 64 82.88V448H16c-8.875 0-16 7.125-16 15.1v31.1C0 504.9 7.125 512 16 512H320V33.13C320 11.63 300.5-4.243 280.3 1.007zM232 288c-13.25 0-24-14.37-24-31.1c0-17.62 10.75-31.1 24-31.1S256 238.4 256 256C256 273.6 245.3 288 232 288z"/></svg>
+          <br>
+            &copy QUORASIUM.COM
+        </a>
+      </div>
+      <div id="sitemap" class="col list-group">
+        <h5 class="mb-3">MENÚ</h5>
+        <ul class="list-unstyled">
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">SOBRE NOSOTROS</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">POLÍTICA DE PRIVACIDAD</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">TÉRMINOS Y CONDICIONES</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">PREGUNTAS FRECUENTES</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">LIBROS DE DOMINIOS PÚBLICO</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">MAPA DEL SITIO</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">RANKINGS</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">GÉNEROS</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">BUSCAR</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">NUESTRAS LISTAS PÚBLICAS</a>
+          </li>
+        </ul>
+      </div>
+      <div id="topBookList" class="col list-group">
+        <h5 class="mb-3">LIBROS MÁS LEÍDOS</h5>
+        <ul class="list-unstyled">
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">EL PRINCIPITO</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">CIEN AÑOS DE SOLEDAD</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">HARRY POTTER Y LA PIEDRA FILOSOFAL</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">CRÓNICA DE UNA MUERTE ANUNCIADA</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">LA METAMORFOSIS</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">BAJO LA MISMA ESTRELLA</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">ORGULLO Y PREJUICIO</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">EL AMOR EN TIEMPOS DE CÓLERA</a>
+          </li>
+          <li class="mb-2">
+            <a href="#" class="text-decoration-none text-dark">LAS MIL Y UNA NOCHES</a>
+          </li>
+        </ul>
+      </div>
+      <div id="socialLinks" class="col">
+        <h5 >SEGUIR</h5>
+
+      </div>
+
+      <div id="BenginLinks" class="col">
+        <h5 class="mb">COMENZAR</h5>
+    
+      </div>
+      </div>
+      </div>
+    
---
+++ includes/header.php
@@ -0,0 +1,59 @@
+
+  <div class="promo d-flex mx-auto">
+    <div class="carousel slide m-1 flex-grow-1" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="2000">
+          <a href="#">Envío gratis a partir de 20€</a>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <a href="#">5% de descuento</a>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <a href="#">Otro descuento</a>
+        </div>
+      </div>
+    </div>
+    <div class="m-1">
+      <a href="#">Blog</a>
+    </div>
+    <div class="m-1">
+      <a href="#">Estado de tu pedido</a>
+    </div>
+    <div class="m-1">
+      <a href="#">Ayuda</a>
+    </div>
+    <div class="m-1">
+      <a href="#">Venta a empresa e instituciones</a>
+    </div>
+  </div>
+  
+  <div class="navbar navbar-primary bg-primary">
+
+      <div class="promo container d-flex">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <svg width="36px" height="48px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M560 448H512V113.5c0-27.25-21.5-49.5-48-49.5L352 64.01V128h96V512h112c8.875 0 16-7.125 16-15.1v-31.1C576 455.1 568.9 448 560 448zM280.3 1.007l-192 49.75C73.1 54.51 64 67.76 64 82.88V448H16c-8.875 0-16 7.125-16 15.1v31.1C0 504.9 7.125 512 16 512H320V33.13C320 11.63 300.5-4.243 280.3 1.007zM232 288c-13.25 0-24-14.37-24-31.1c0-17.62 10.75-31.1 24-31.1S256 238.4 256 256C256 273.6 245.3 288 232 288z"/></svg>
+          <strong>QUORASIUM</strong>
+        </a>
+
+
+        <div class="row">
+          <div class="col-auto">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Busca por categoía, autor, género, ISBN">
+              <button class="btn btn-secondary" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg></button>
+            </div>
+          </div>
+        </div>
+
+        
+        <div>
+          <button class="btn btn-dark">
+            <a href="#" class="text-white">Alta socio</a>
+          </button>
+          <button class="btn btn-dark">
+            <a href="#" class="text-white">Identificate</a>
+          </button>
+        </div>
+
+      </div>
+    </div>
